59f13a57605e57978d1e6cb07081953b972774d0
colormethanh <thnguyen1847@gmail.com>
2023-05-03 22:50:49 +0900

    new commit

--- frontend/src/recipeComponents/home.js
+++ frontend/src/recipeComponents/home.js
@@ -91,7 +91,7 @@ function GalleryCard(props) {
             <Card className="gallery-card" onClick={handleOnClick}>
                 <CardImg
                     alt="card image"
-                    src={props.recipe.image}
+                    src={props.recipe.image.url}
                     className="card-img"
                 />
                 <CardImgOverlay>
